e17a4428490b982ca62f9e427f7958e0b9e3b40e
RiikkaKilp <RiikkaKilp@users.noreply.github.com>
2021-01-31 12:01:37 +0200

    add font toggle

---
+++ FGJ/Assets/Scripts/Utils/ChangeFont.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ChangeFont : MonoBehaviour
+{
+    public TMP_Text[] allText;
+    public TMP_FontAsset defaultFont;
+    public TMP_FontAsset simpleFont;
+    public Toggle toggle;
+
+    public void ChangeToSimple()
+    {
+        for (int i = 1; i < allText.Length; i++)
+        {
+            allText[i].font = toggle.isOn ? simpleFont : defaultFont;
+            allText[i].color = Color.black;
+        }
+    }
+}
