d23e6a27083791093f56c6d107d3a6b215980cd6
maukii <maunokiiskinen@kamk.fi>
2021-01-31 13:51:02 +0200

    Added inverted option to lookAt script and example interactable object

---
+++ FGJ/Assets/Scripts/Utils/Interactable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Interactable : MonoBehaviour
+{
+    [Tooltip("Add popup text here")]
+    [TextArea(1, 6)]
+    public string text = string.Empty;
+    [SerializeField] private TMP_Text textUI;
+    private bool visible = false;
+
+
+    private void OnValidate()
+    {
+        if(textUI != null)
+        {
+            textUI.SetText(text);
+        }
+    }
+
+    private void Awake()
+    {
+        textUI = GetComponentInChildren<TMP_Text>();
+        textUI.SetText(text);
+        textUI.enabled = visible;
+    }
+
+    // OnClicked event
+    private void OnMouseDown()
+    {
+        visible = !visible;
+        textUI.enabled = visible;
+    }
+}
--- FGJ/Assets/Scripts/Utils/LookAt.cs
+++ FGJ/Assets/Scripts/Utils/LookAt.cs
@@ -1,14 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LookAt : MonoBehaviour
 {
     [SerializeField] private Transform target = null;
+    [SerializeField] private bool inverted = false;
+    private Camera camera;
 
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
 
     private void Update()
     {
-        transform.LookAt(target);    
+        if (inverted)
+        {
+            transform.LookAt(transform.position + camera.transform.rotation * Vector3.forward, camera.transform.rotation * Vector3.up);
+        }
+        else
+        {
+            transform.LookAt(target);
+        }
     }
 }
