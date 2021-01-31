0e4eb56ddd74a1e88f8965372fb5b54428179fb4
maukii <maunokiiskinen@kamk.fi>
2021-01-31 15:41:09 +0200

    Add small highlighting when hovering over letters

--- FGJ/Assets/Scripts/Utils/Interactable.cs
+++ FGJ/Assets/Scripts/Utils/Interactable.cs
@@ -9,6 +9,12 @@ public class Interactable : MonoBehaviour
     [SerializeField] private TMP_Text textUI;
     private bool visible = false;
 
+    [Header("Highlighting")]
+    [SerializeField] private float defaultSize = 0.015f;
+    [SerializeField] private float highlightedSize = 0.03f;
+    private MeshRenderer rend;
+    private MaterialPropertyBlock block;
+
 
     private void OnValidate()
     {
@@ -23,6 +29,28 @@ public class Interactable : MonoBehaviour
         textUI = GetComponentInChildren<TMP_Text>();
         textUI.SetText(text);
         textUI.enabled = visible;
+
+        if (block == null)
+            block = new MaterialPropertyBlock();
+    }
+
+    private void Highlight(bool enabled)
+    {
+        rend = transform.Find("PointCloud").GetComponent<MeshRenderer>();
+        rend.GetPropertyBlock(block);
+        block.SetFloat("_RandSize", !enabled ? defaultSize : highlightedSize);
+        block.SetFloat("_PointSize", !enabled ? defaultSize : highlightedSize);
+        rend.SetPropertyBlock(block);
+    }
+
+    private void OnMouseEnter()
+    {
+        Highlight(true);
+    }
+
+    private void OnMouseExit()
+    {
+        Highlight(false);
     }
 
     // OnClicked event
